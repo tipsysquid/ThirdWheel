47ca86bb6eb5c1104e30142759fe60b2bdc6268d
Ken Gaulter <ken@tipsysquid.io>
2018-03-28 01:46:47 -0500

    Key model and controller

---
+++ app/controllers/key.js
@@ -0,0 +1,142 @@
+const crypto = require('crypto');
+var Q = require('q');
+var mongoose = require('mongoose');
+var Key = mongoose.model('Key');
+var account = require('../controllers/account.js');
+const server_token = '662fc8d3-1067-46e2-a28d-c52900e76078';
+
+exports.addKey = function(req, res, callback){
+    var deferred = Q.defer();
+    var client_obs;
+
+    verifyAddKeyInputs(req, res)
+    .then(function(verified){
+        client_obs = {
+            client_email:req.body.email,
+            client_key:req.body.key
+        };
+        return;
+    })
+    .then(function(client_obs){
+        var inner_deferred = Q.defer();
+        req.server_token = server_token;
+        authAccount(req, res)
+        .then(function(saved_account){
+            inner_deferred.resolve(saved_account);
+        })
+        .catch(function(ex){
+            inner_deferred.reject(ex);
+        });
+        return inner_deferred.promise;
+    })
+    .then(function(saved_account){
+        var inner_deferred = Q.defer();
+        createKey(saved_account, client_obs)
+        .then(function(saved_key){
+            inner_deferred.resolve(saved_key);
+        })
+        .catch(function(ex){
+            inner_deferred.reject(ex);
+        });
+        return inner_deferred.promise;
+    })
+    .then(function(saved_key){
+        deferred.resolve(
+            res.status(200).send({message:'Your key has been saved'})
+        );
+    })
+    .catch(function(ex){
+        deferred.reject(
+            res.status(400).send({message:'Your key was not saved'})
+        );
+    });
+
+    deferred.promise.nodeify(callback);
+    return deferred.promise;
+}
+
+function authAccount(req, res, callback){
+    var deferred = Q.defer();
+    req.server_token = server_token;
+    account.login(req, res)
+    .then(function(saved_account){
+        var inner_deferred = Q.defer();
+        if(saved_account){
+            inner_deferred.resolve(saved_account);
+        }
+        else{
+            inner_deferred.reject({message:'No account found'});
+        }
+        return inner_deferred.promise;
+    })
+    .then(function(res){
+        deferred.resolve(res);
+    })
+    .catch(function(ex){
+        deferred.reject(ex);
+    });
+
+    deferred.promise.nodeify(callback);
+    return deferred.promise;
+}
+
+function verifyAddKeyInputs(req, res, callback){
+    var deferred = Q.defer();
+    if(!req.body){
+        deferred.reject(
+            res.status(400).send({message:"No data"})
+        );
+    }
+    else{
+        if(!req.body.email){
+            deferred.reject(
+                res.status(400).send({message:"You must provide an email"})
+            );
+        }
+        else{
+            if(!req.body.password){
+                deferred.reject(
+                    res.status(400).send({message:"You must provide your password"})
+                );
+            }
+            else{
+                if(!req.body.key){
+                    deferred.reject(
+                        res.status(400).send({message:"Key cannot be empty"})
+                    );
+                }
+                else{
+                    deferred.resolve({});
+                }
+            }
+        }
+    }
+    deferred.promise.nodeify(callback);
+    return deferred.promise;
+}
+
+/**
+ * 
+ * @param {*} auth_account 
+ * @param {*} client_obs 
+ */
+function createKey(auth_account, client_obs, callback){
+    var deferred = Q.defer();
+
+    var key = new Key({
+        account:auth_account,
+        key:client_obs.client_key,
+        created_at:(new Date()).toUTCString()
+    });
+    key.save(key)
+    .then(function(saved_key){
+        console.log("created key!");
+        deferred.resolve(saved_key);
+    })
+    .catch(function(ex){
+        deferred.reject(JSON.stringify(ex));
+    });
+
+    deferred.promise.nodeify(callback);
+    return deferred.promise;
+}
---
+++ app/models/Key.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose'); //for schema
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+
+var KeySchema = new mongoose.Schema({
+    account: {
+        type: ObjectId,
+        ref: "Account",
+        required: true,
+        trim: true,
+        unique: true
+    },
+    key: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    created_at: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Key', KeySchema);
